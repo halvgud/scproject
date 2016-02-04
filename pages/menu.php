2cb6f0c897da4b2dc3c48fbd4111cd0d6c5b00e6
Rafael Alarcón <rafaalata@gmail.com>
2016-02-04 00:19:34 -0700

    add icons
    add icons to the menu items

--- pages/menu.php
+++ pages/menu.php
@@ -39,42 +39,41 @@
                             <a href="index.php"><i class="fa fa-home fa-fw"></i> Home</a>
                         </li>
                         <li>
-                            <!-- <i class="fa fa-bar-chart-o fa-fw"></i> -->
-                            <a href="#"><i class="fa fa-dashboard fa-fw"></i> Administrador<span class="fa arrow"></span></a>
+                            <a href="#"><i class="fa fa-users fa-fw"></i> Administrador<span class="fa arrow"></span></a>
                             <ul class="nav nav-second-level">
                                 <li>
-                                    <a href="admin-alta-user.php">Alta</a>
+                                    <a href="admin-alta-user.php"><i class="fa fa-plus fa-fw"></i> Alta</a>
                                 </li>
                                 <li>
-                                    <a href="admin-modificar-user.php">Modificar</a>
+                                    <a href="admin-modificar-user.php"><i class="fa fa-pencil fa-fw"></i> Modificar</a>
                                 </li>
                                 <li>
-                                    <a href="admin-eliminar-user.php">Eliminar</a>
+                                    <a href="admin-eliminar-user.php"><i class="fa fa-minus fa-fw"></i> Eliminar</a>
                                 </li>
                             </ul>
                             <!-- /.nav-second-level -->
                         </li>
                         <li>
                             <!-- <i class="fa fa-bar-chart-o fa-fw"></i> -->
-                            <a href="#"><i class="fa fa-dashboard fa-fw"></i> Control de Medicamentos<span class="fa arrow"></span></a>
+                            <a href="#"><i class="fa fa-medkit fa-fw"></i> Control de Medicamentos<span class="fa arrow"></span></a>
                             <ul class="nav nav-second-level">
                                 <li>
-                                    <a href="altauser.php">Consultas</a>
+                                    <a href="altauser.php"><i class="fa fa-stethoscope fa-fw"></i> Consultas</a>
                                 </li>
                                 <li>
-                                    <a href="morris.html">Pase de salida</a>
+                                    <a href="morris.html"><i class="fa fa-file-text-o fa-fw"></i> Pase de salida</a>
                                 </li>
                                 <li>
-                                    <a href="#">Medicamento <span class="fa arrow"></span></a>
+                                    <a href="#"><i class="fa fa-plus-square fa-fw"></i> Medicamento <span class="fa arrow"></span></a>
                                     <ul class="nav nav-third-level">
                                         <li>
-                                            <a href="#">Nuevo</a>
+                                            <a href="#"><i class="fa fa-plus fa-fw"></i> Nuevo</a>
                                         </li>
                                         <li>
-                                            <a href="#">Actualizar</a>
+                                            <a href="#"><i class="fa fa-pencil fa-fw"></i> Actualizar</a>
                                         </li>
                                         <li>
-                                            <a href="#">Eliminar</a>
+                                            <a href="#"><i class="fa fa-minus fa-fw"></i> Eliminar</a>
                                         </li>
                                     </ul>
                                     <!-- /.nav-third-level -->
@@ -84,29 +83,42 @@
                         </li>
                         <li>
                             <!-- <i class="fa fa-bar-chart-o fa-fw"></i> -->
-                            <a href="#"><i class="fa fa-dashboard fa-fw"></i> Expediente M&eacute;dico<span class="fa arrow"></span></a>
+                            <a href="#"><i class="fa fa-file-text fa-fw"></i> Expediente M&eacute;dico<span class="fa arrow"></span></a>
                             <ul class="nav nav-second-level">
                                 <li>
-                                    <a href="altauser.php">Consultas</a>
+                                    <a href="altauser.php"><i class="fa fa-stethoscope fa-fw"></i> Consultas</a>
                                 </li>
                                 <li>
-                                    <a href="morris.html">Expediente</a>
+                                    <a href="morris.html"><i class="fa fa-file-text fa-fw"></i> Expediente</a>
                                 </li>
                                 <li>
-                                    <a href="morris.html">Pase de salida</a>
+                                    <a href="morris.html"><i class="fa fa-file-text-o fa-fw"></i> Pase de salida</a>
                                 </li>
                             </ul>
                             <!-- /.nav-second-level -->
                         </li>
                         <li>
                             <!-- <i class="fa fa-bar-chart-o fa-fw"></i> -->
-                            <a href="#"><i class="fa fa-dashboard fa-fw"></i> Incapacidades<span class="fa arrow"></span></a>
+                            <a href="#"><i class="fa fa-ambulance fa-fw"></i> Incapacidades<span class="fa arrow"></span></a>
                             <ul class="nav nav-second-level">
                                 <li>
-                                    <a href="altauser.php">Incapacidad</a>
+                                    <a href="altauser.php"><i class="fa fa-file-o fa-fw"></i> Incapacidad</a>
                                 </li>
                                 <li>
-                                    <a href="morris.html">Memo</a>
+                                    <a href="morris.html"><i class="fa fa-file fa-fw"></i> Memo</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <!-- <i class="fa fa-bar-chart-o fa-fw"></i> -->
+                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i></i> Reportes<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="altauser.php"><i class="fa fa-stethoscope fa-fw"></i> Consultas</a>
+                                </li>
+                                <li>
+                                    <a href="morris.html"><i class="fa fa-user fa-fw"></i> Visitas</a>
                                 </li>
                             </ul>
                             <!-- /.nav-second-level -->
