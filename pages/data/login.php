31ab81ab90d9e0bc5142214025da9c1632309824
Rafael Alarcón <rafaalata@gmail.com>
2016-02-09 00:37:50 -0700

    quitar lineas al login

--- pages/data/login.php
+++ pages/data/login.php
@@ -17,8 +17,28 @@ if(isset($data) && isset($data->usuario) && $data->usuario != "" && isset($data-
 	$cn = new Conexion();
 	$cn->abrirConexion();
 	$cn->seleccion('usuario','id_usuario,usuario,rol',NULL,'password="'.$data->password.'" and usuario = "'.$data->usuario.'"','id_usuario DESC');
-	$res = $cn->obtenerResultadoJson();
-	print_r($res);
+
+	if($cn->numeroRegistros()>0){
+		$res = $cn->obtenerResultado();
+		// $_SESSION['id_usuario']= $res[0]['id_usuario'];
+		// $_SESSION['usuario'] = $res[0]['usuario'];
+		// $_SESSION['rol'] = $res[0]['rol'];
+		print $cn->obtenerResultadoJson();
+	}
+	else{
+		http_response_code(403);
+		$resArray = array();
+		$resArray['error'] = 'Usuario o Password incorrecto, por favor introduzca los datos correctos.';
+		// var_dump($resArray);
+		print json_encode($resArray);
+	}
+}
+else{
+	$resArray = array();
+	http_response_code(403);
+		$resArray['error'] = 'Datos insuficientes, debe introducir usuario y password';
+		// var_dump($resArray);
+		print json_encode($resArray);
 }
 
 ?>
